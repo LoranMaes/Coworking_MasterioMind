8bce46c4d16cd165f39ed4004bfa562a21f21fc1
Loran Maes <loran.maes@gmail.com>
2022-05-06 09:37:36 +0200

    commit local branch ik weet nie wa

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,11 @@
-"use strict"
-
 (function(){
+    "use strict"
+    const starten = document.querySelector("#starten_btn")
+    const spelregels = document.querySelector("#spelregels_btn")
+    const opties = document.querySelector("#opties_btn")
+    const taal = document.querySelector("#taal_btn")
 
+    spelregels.addEventListener("click", () => {
+        window.location = "./pages/spelregels"
+    })
 })()
