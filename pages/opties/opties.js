546579e4e35e6ec95ee2d85d6fc85bcd2ac20548
ThiboPresident <95861130+ThiboPresident@users.noreply.github.com>
2022-05-06 10:56:15 +0100

    Aanpassingen opties

--- pages/opties/opties.js
+++ pages/opties/opties.js
@@ -27,15 +27,25 @@
         bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
     }
 
+    function setVolume() {
+        let audio = document.getElementById("audio");
+        let volume = range.value;
+        console.log(volume)
+        audio.volume = volume;
+    }
 
     document.getElementById("muziek").addEventListener("click", e => {let checkBox = document.getElementById("muziek");
         let text = document.getElementById("muziekstatus");
+        let audio = document.getElementById("audio");
 
         if (checkBox.checked === true){
             text.innerHTML = "(enabled)";
+            audio.play();
         }
         else {
             text.innerHTML = "(disabled)";
+            audio.pause();
+            audio.currentTime = 0;
         }
     })
 })()
