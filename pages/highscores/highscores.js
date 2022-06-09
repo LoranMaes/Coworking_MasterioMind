17a2b29aa2e82e6dda6ed388770cb3304dcac0e6
DownieTown <lukas.downes@student.odisee.be>
2022-06-09 18:34:41 +0200

    fuck dit man

--- pages/highscores/highscores.js
+++ pages/highscores/highscores.js
@@ -6,4 +6,28 @@
     close.addEventListener("click", () => {
         window.location = "../../"
     })
+
+    const scoreList = document.querySelector("ol");
+    const getAllScores = async () => {
+        try {
+            const result = await fetch("https://backend.lukasdownes.ikdoeict.be/masteriomind");
+            if(!result.ok) throw Error()
+            const data = await result.json();
+            maakLijst(data.data);
+        } catch(err) {
+            console.log(err)
+        }
+    }
+    const maakLijst = (scores) => {
+        scores.forEach(el => {
+            let li = document.createElement("li");
+            let htmlText = `<h2>${el.name}</h2><span>${el.score}</span>`
+            li.innerHTML = htmlText;
+            scoreList.appendChild(li);
+        });
+    }
+    getAllScores();
 })()
+
+//TO DO: transelate
+// rules correction 
